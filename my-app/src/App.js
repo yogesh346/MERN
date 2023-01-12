a64ef20fae3628c5bc54788b2146bf607c83c67e
yogesh346 <yogeshdhiman346@gmail.com>
2023-01-12 12:25:06 +0530

    workdone

---
+++ my-app/src/App.js
@@ -0,0 +1,48 @@
+//import React from 'react';
+//import logo from './logo.svg';
+//import'./App.css';
+//import Profile from './Container/HomeClass/index.js'
+//function App() {
+ // return (
+  //  <div className="App">
+    //  <header className="App-header">
+//<Profile/>
+   //   </header>
+   // </div>
+
+ // );
+//
+//const json ='{"result":true,"count":42}';
+//const obj = JSON.parse(json);
+//console.log(obj.count)
+
+//console.log(JSON.stringify({x:5,y:6}));
+
+//let my obj = {
+ // "a":100,
+ // "b": 200,
+ // "c":300
+//};
+//console.log(Object.keys(my obj));
+
+const object1 = {
+  a: '100',
+  b: 42,
+  c: 34
+};
+
+console.log(Object.values(object1));
+/*
+let a = {
+  "a".100,
+  "b".200,
+  "c".300
+
+  let b = {
+    "d":400,
+    "e":500,
+    "f":600
+  };
+  console.log(Object.assign(a,b));
+}
+*/
