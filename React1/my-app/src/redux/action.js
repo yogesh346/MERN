3ff810c1003aab476d4a5452d86065a71b67372c
yogesh346 <yogeshdhiman346@gmail.com>
2023-02-03 12:35:49 +0530

    msg

--- React1/my-app/src/redux/action.js
+++ React1/my-app/src/redux/action.js
@@ -1,8 +1,9 @@
+export {ADD_TO_CART,REMOVE_TO_CART} from "./constant"
 export const addtoCart = (data) => {
 
     console.warn("action called",data)
     return {
-        type:'Add_TO_CART',
+        type:'REMOVE_TO_CART',
         data:"1 item"
     }
 }
--- React1/my-app/src/redux/reducer.js
+++ React1/my-app/src/redux/reducer.js
@@ -1,4 +1,16 @@
+import {ADD_TO_CART,REMOVE_TO_CART} from "./constant"
 export const cartData = (data = [],action) => {
-    console.warn("reducer",action)
-    return "abc"
+    switch(action.type){
+        case ADD_TO_CART:
+            //add to cart logic
+            console.warn("ADD_TO_CART CONDITION",action)
+            return 1+1;
+                case REMOVE_TO_CART:
+                    //add to cart logic
+                    console.warn("REMOVE_TO_CART CONDITION",action)
+                    return 1-1;
+            default:
+                //no case matched
+                return 'no action called'
+    }
 }
